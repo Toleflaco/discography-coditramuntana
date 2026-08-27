6e8b08236e1b02fc01601fe3df3f8ea6d10a4d37
Tole <manutol@gmail.com>
2026-08-27 07:26:29 +0200

    chore: initial Spring Boot scaffolding / estructura inicial Spring Boot

---
+++ src/test/java/com/coditramuntana/discography/DiscographyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coditramuntana.discography;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscographyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
